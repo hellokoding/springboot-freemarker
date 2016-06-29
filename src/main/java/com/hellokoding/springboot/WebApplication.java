f6820fc77bdc99e27926f25199449c4bec22fb99
Giau <giaunv@users.noreply.github.com>
2016-06-29 14:42:32 +0700

    FreeMarker

--- src/main/java/com/hellokoding/springboot/WebApplication.java
+++ src/main/java/com/hellokoding/springboot/WebApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class WebApplication extends SpringBootServletInitializer {
-
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(WebApplication.class);
